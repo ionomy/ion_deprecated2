4e94d101dca05bd50a5a2fe1bea7921ed48adbae
Matlack <adammatlack@users.noreply.github.com>
2017-02-12 16:01:49 -0600

    Update version.h

--- src/version.h
+++ src/version.h
@@ -53,7 +53,7 @@ static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 95050;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 95600;
+static const int CADDR_TIME_VERSION = 95050;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
